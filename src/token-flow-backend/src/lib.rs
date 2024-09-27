68397edeebdd0ece59da262b73dd1e470082962b
jyotirmaygithub <jyotirmay2000gupta@gmail.com>
2024-09-27 15:21:00 +0530

    feat : functions - for minting, transfer tokens and to burn tokens

--- src/token-flow-backend/src/lib.rs
+++ src/token-flow-backend/src/lib.rs
@@ -1,6 +1,9 @@
 mod profile_creation;
 mod stable;
 
+use core::num;
+use std::sync::mpsc::Receiver;
+
 use ic_cdk::{export_candid, query, update};
 use profile_creation::User;
 use stable::{mutate_state, read_state};
@@ -33,13 +36,6 @@ pub fn add_user_profile(user_id: String, user: User) {
     }
 }
 
-#[query]
-pub fn get_user_profile(user_id: String) -> Option<User> {
-    let caller = ic_cdk::api::caller();
-    ic_cdk::println!("pricipal id = {}", caller);
-    read_state(|state| state.user_profiles.get(&user_id).clone())
-}
-
 #[update]
 pub fn assign_tokens(user_id: String, amount: i32) {
     let caller = ic_cdk::api::caller();
@@ -67,6 +63,64 @@ pub fn assign_tokens(user_id: String, amount: i32) {
             .insert(caller.to_string(), admin_profile);
         state.user_profiles.insert(user_id.clone(), user_profile);
     });
+}
+
+#[update]
+pub fn minter() {
+    let minted_tokens = 100000;
+    let caller = ic_cdk::api::caller();
+    let mut admin_profile =
+        get_user_profile(caller.to_string()).expect("Admin does not exist to assign tokens!");
+    admin_profile.tokens += minted_tokens;
+
+    // Update the state with the new minted tokens.
+    mutate_state(|state| {
+        state
+            .user_profiles
+            .insert(caller.to_string(), admin_profile);
+    });
+}
+
+#[update]
+pub fn transfer_tokens(sender: String, reciver: String, number_tokens: i32) {
+    let mut sender_profile = get_user_profile(sender.clone()).expect("sender does not exist!");
+    let mut receiver_profile = get_user_profile(reciver.clone()).expect("reciver does not exist!");
+
+    if sender_profile.tokens < number_tokens {
+        panic!("sender does not have sufficenet amount of tokens to send");
+    }
+
+    sender_profile.tokens -= number_tokens;
+    receiver_profile.tokens += number_tokens;
+
+    // Update the state with the new tokens.
+    mutate_state(|state| {
+        state.user_profiles.insert(sender, sender_profile);
+        state.user_profiles.insert(reciver, receiver_profile);
+    });
+}
+
+#[update]
+pub fn burn_tokens(burner_id: String, number_tokens: i32) {
+    let mut burner_profile = get_user_profile(burner_id.clone()).expect("user does not exist!");
+    if burner_profile.tokens < number_tokens {
+        panic!("user does not have sufficenet amount of tokens to burn");
+    }
+
+    burner_profile.tokens -= number_tokens;
+
+    // Update the state with the new tokens.
+    mutate_state(|state| {
+        state.user_profiles.insert(burner_id, burner_profile);
+    });
+    
+}
+
+#[query]
+pub fn get_user_profile(user_id: String) -> Option<User> {
+    let caller = ic_cdk::api::caller();
+    ic_cdk::println!("pricipal id = {}", caller);
+    read_state(|state| state.user_profiles.get(&user_id).clone())
 }
 
 export_candid!();
